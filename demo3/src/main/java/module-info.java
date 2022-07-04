2946e44463ee192416183d561228b6290868f329
Zahra <zahraamasoumi@gmail.com>
2022-07-04 16:40:03 +0430

    extra files - libraries

---
+++ demo3/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.demo3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.java;
+
+
+    opens com.example.demo3 to javafx.fxml;
+//    exports com.example.demo3;
+    exports com.example.demo3.GUI;
+    opens com.example.demo3.GUI to javafx.fxml;
+}
